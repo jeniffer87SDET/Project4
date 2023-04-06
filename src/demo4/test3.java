f8e7808f23a00fa4d848a41280da6d8ab7b5d01e
Rimsky <Rimsky@DESKTOP-BO2IQFC.lan>
2023-04-05 22:49:27 -0400

    "Initial Commit"

---
+++ src/demo4/test3.java
@@ -0,0 +1,13 @@
+package demo4;
+
+public class test3 {
+
+	
+	public static void  main (String args []) {
+		
+		System.out.println("Test 4...");
+		
+		System.out.println("Practice Test 4...");
+	}
+
+}
